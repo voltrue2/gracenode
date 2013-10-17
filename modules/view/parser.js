c85f345f7e6681696d08cea2811a51e9f3b7cff9
voltrue2 <voltrue2@gmail.com>
2013-10-17 14:18:45 +0900

    parser.js added to view module

---
+++ modules/view/parser.js
@@ -0,0 +1,76 @@
+
+var gracenode = require('../../gracenode');
+var log = gracenode.log.create('view/parser');
+
+module.exports.create = function (valueMap) {
+	return new Parser(valueMap);
+};
+
+function Parser(valueMap) {
+	this._valueMap = valueMap || null;
+}
+
+Parser.prototype.parseData = function (data) {
+	var includeList = [];
+	data = this.parse('(:', ':)', data, function (that, tag, keyTag, indicator, data) {
+		// evaluate the indicator
+		switch (indicator) {
+			case '=':
+				data = that.replace(that, tag, keyTag, indicator, data);
+				break;
+			case 'include':
+				includeList.push({ tag: tag, path: keyTag });
+				break;
+			default:
+				log.warning('unkown indicator found: [' + indicator + '] ' + tag);
+				break;
+		}
+		return data;
+	});
+	return {
+		includeList: includeList,
+		data: data
+	};
+};
+
+// functions below are not meant to be used outside of this file
+
+Parser.prototype.parse = function (opening, closing, data, callbackEach) {
+	var openIndex = data.indexOf(opening);
+    var closeIndex = data.indexOf(closing);
+    var tmp = data;
+	while (openIndex !== -1 && closeIndex !== -1) {
+        //extract tag for replacement
+		var closer = closeIndex + 2;
+		var tag = tmp.substring(openIndex, closer);
+		var indicator = tag.substring(2, tag.indexOf(' '));
+        var keyTag = tag.substring(2 + indicator.length, tag.length - 2).trim(' ');
+		// update tmp
+		tmp = tmp.substring(closer);	
+		
+		// replace
+		data = callbackEach(this, tag, keyTag, indicator, data);
+        
+		// move forward
+		openIndex = tmp.indexOf(opening);
+        closeIndex = tmp.indexOf(closing);
+    }
+    return data;
+};
+
+Parser.prototype.replace = function (that, tag, keyTag, indicator, data) {
+	var keys = keyTag.split('.');
+	if (that._valueMap[keys[0]] !== undefined) {
+		var value = that._valueMap[keys[0]];
+		if (typeof value === 'object') {
+			for (var i = 0, len = keys.length; i < len; i++) {
+				if (value[keys[i]] !== undefined) {
+					value = value[keys[i]];
+				}
+			}
+		}
+		return data.replace(tag, value);
+	}
+	log.warning('no value found for tag "' + tag + '"');
+	return data;
+};
