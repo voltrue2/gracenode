e5b18082f4ab066fe55e8a492dc7d6f2e5683b2d
voltrue2 <voltrue2@gmail.com>
2014-04-14 12:09:40 +0900

    log module updated now uses proper EventEmitter

--- modules/log/lib/msg.js
+++ modules/log/lib/msg.js
@@ -9,15 +9,11 @@ module.exports.create = function (prefix, logName, levelName, args) {
 	var ymd = date.getFullYear() + '/' + pad(date.getMonth() + 1, 2) + '/' + pad(date.getDate(), 2);
 	var his = pad(date.getHours(), 2) + ':' + pad(date.getMinutes(), 2) + ':' + pad(date.getSeconds(), 2) + ':' + pad(date.getMilliseconds(), 3); 
 	var timestamp = ymd + ' ' + his;
-	var space = '';
-	for (var i = 0, len = timestamp.length; i < len; i++) {
-		space += ' ';
-	}
-	var msg = [color.create(levelName, (prefix ? '[' + prefix + '] ' : '') + '[' + timestamp + '] <' + levelName + '> [' + logName + ']')];
+	var msg = [(prefix ? '[' + prefix + '] ' : '') + '[' + timestamp + '] <' + levelName + '> [' + logName + ']'];
 	for (var key in args) {
-		msg.push(color.create(levelName, args[key], space));
+		msg.push(args[key]);
 	}
-	return { message: msg.join(' '), timestamp: date.getTime() };
+	return { message: color.create(levelName, msg.join(' ')), timestamp: date.getTime() };
 };
 
 function pad(n, digit) {
