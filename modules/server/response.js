3e9701b864eb985caef8e625597108428a47a979
voltrue2 <voltrue2@gmail.com>
2014-04-30 17:28:02 +0900

    removed unnecessary log from server module

--- modules/server/response.js
+++ modules/server/response.js
@@ -7,8 +7,6 @@ var zlib = require('zlib');
 // set up exception catcher and handle if an exception is caught
 module.exports.setupExceptionHandler = function (req, res) {
 
-	log.info('respond on exception is set');
-
 	var errorCallback = function (error) {
 		
 		log.error('exception caught(url:' + req.url + '): ', error);
