36721da9b28df6319ca12c7dd1ca2c12d31b52f5
voltrue2 <voltrue2@gmail.com>
2014-06-12 10:29:37 +0900

    removed unnecessary error.stack from uncaught exception log

--- core/index.js
+++ core/index.js
@@ -293,7 +293,7 @@ function handleShutdownTasks(that, cb) {
 function setupListeners(that) {
 	
 	process.on('uncaughtException', function (error) {
-		log.fatal('gracenode detected an uncaught exception', error, error.stack);
+		log.fatal('gracenode detected an uncaught exception:', error);
 		that.emit('uncaughtException', error);
 	});
 
