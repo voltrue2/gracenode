84093d2cb5628daf6d45c58b84c9f842b9b9a140
voltrue2 <voltrue2@gmail.com>
2014-07-29 19:01:07 +0900

    removed unnecessary code

--- scripts/daemon/restart.js
+++ scripts/daemon/restart.js
@@ -5,26 +5,6 @@ var lib = require('./utils/lib');
 var talk = require('./utils/talk');
 
 module.exports = function (path) {
-	/*
-	var sockFile = sockName(path);
-	fs.exists(sockFile, function (exists) {
-		if (!exists) {
-			logger.error(lib.color('daemon process ' + path + ' not running', lib.COLORS.RED));
-			return gn.exit(new Error('processNotFound'));
-		}
-		gn.on('uncaughtException', function (error) {
-			logger.error(lib.color(path + ' ' + sockName(path), lib.COLORS.RED));
-			gn.exit(error);
-		});
-		var sock = new net.Socket();
-		sock.connect(sockFile, function () {
-			sock.write('restart');
-			console.log(lib.color('Daemon process restarted', lib.COLORS.GRAY), lib.color(path, lib.COLORS.LIGHT_BLUE));
-			console.log(lib.color('Restarting multiple times in quick succession (within 10 seconds) is', lib.COLORS.GRAY) + lib.color(' NOT ', lib.COLORS.BROWN) + lib.color('allowed', lib.COLORS.GRAY));
-			gn.exit();
-		});
-	});
-	*/
 	// listener for exceptions
 	gn.on('uncaughtException', function (error) {
 		logger.error(lib.color(path + ' ' + sockName(path), lib.COLORS.RED));
