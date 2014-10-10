faed69f1b8ae24eebf92ffa3a2eff78b7eb63ace
voltrue2 <voltrue2@gmail.com>
2014-10-10 11:31:00 +0900

    lint error of configs format changed

--- modules/config/index.js
+++ modules/config/index.js
@@ -136,7 +136,7 @@ function lintConfig(data) {
 			// there is an lint error
 			var errors = jshint.data().errors;
 			for (var i = 0, len = errors.length; i < len; i++) {
-				console.error('*** LINT ERROR [error] Line', errors[i].line, 'Character', errors[i].character, errors[i].reason);
+				console.error('    *** LINT [error] Line', errors[i].line, 'Character', errors[i].character, errors[i].reason);
 			}
 		}	
 	}
