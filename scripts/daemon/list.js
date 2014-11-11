baa2efb8a91bc1cf21c79db32cf8ef938d2093d3
voltrue2 <voltrue2@gmail.com>
2014-11-11 11:18:58 +0900

    added executed user and application path to daemon list ouput

--- scripts/daemon/list.js
+++ scripts/daemon/list.js
@@ -1,36 +1,59 @@
 'use strict';
 
 var fs = require('fs');
+var exec = require('child_process').exec;
 var gn = require('gracenode');
 var async = require('async');
 var lib = require('./utils/lib');
 var talk = require('./utils/talk');
+
+var TMP_PATH = '/tmp/';
+var GN_MON = 'gracenode-monitor-';
 
 module.exports = function () {
 	var apps = [];
 	var appList = [];
 	// get list of applications
 	var getAppPaths = function (next) {
-		fs.readdir('/tmp/', function (error, list) {
+		fs.readdir(TMP_PATH, function (error, list) {
 			if (error) {
 				return next(error);
 			}
-			for (var i = 0, len = list.length; i < len; i++) {
-				if (list[i].indexOf('gracenode-monitor-') === 0) {
-					apps.push(list[i].replace('gracenode-monitor-', '').replace(/-/g, '/').replace('.sock', ''));
+			async.each(list, function (file, moveOn) {
+				if (file.indexOf(GN_MON) !== 0) {
+					// not gracenode daemon sock
+					return moveOn();
 				}
-			}
-			next();
+				fs.stat(TMP_PATH + file, function (error, stats) {
+					if (error) {
+						return moveOn(error);
+					}
+					apps.push({
+						uid: stats.uid,
+						path: file.replace(GN_MON, '').replace(/-/g, '/').replace('.sock', '')
+					});
+					moveOn();
+				});
+			}, next);
 		});		
+	};
+	// find owner user
+	var findUserForApps = function (next) {
+		async.eachSeries(apps, function (app, moveOn) {
+			exec('awk -v val=' + app.uid + ' -F ":" \'$3==val{print $1}\' /etc/passwd', function (stderr, stdout) {
+				app.user = stdout.replace(/\n/g, '');
+				moveOn();
+			});
+		}, next);
 	};
 	// find applications
 	var findApps = function (next) {
 		async.eachSeries(apps, function (app, moveOn) {
-			talk.findApp(app, function (error, appProcess) {
+			talk.findApp(app.path, function (error, appProcess) {
 				if (error) {
 					return moveOn(error);
 				}
-				appList.push({ app: app, processes: appProcess });
+				appList.push({ uid: app.uid, user: app.user, app: app.path, processes: appProcess });
 				moveOn();
 			});
 		}, next);
@@ -43,6 +66,8 @@ module.exports = function () {
 					return moveOn(error);
 				}
 				console.log('');
+				console.log('	', lib.color('Application path	', lib.COLORS.BROWN), lib.color(appInfo.app, lib.COLORS.LIGHT_BLUE));
+				console.log('	', lib.color('Executed user		', lib.COLORS.BROWN), lib.color(appInfo.user + '(uid:' + appInfo.uid + ')', lib.COLORS.LIGHT_BLUE));
 				for (var i = 0, len = list.length; i < len; i++) {
 					var app = lib.color(list[i].process.replace(process.execPath + ' ', ''), lib.COLORS.GREEN);
 					var pid = lib.color('(' + list[i].pid + ')', lib.COLORS.PURPLE);
@@ -63,6 +88,7 @@ module.exports = function () {
 	// execute the commands
 	async.series([
 		getAppPaths,
+		findUserForApps,
 		findApps,
 		findPids
 	], done);
