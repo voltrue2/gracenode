a11709df4943cc1c99cffbebf9172291dc8488e4
voltrue2 <voltrue2@gmail.com>
2015-10-07 16:01:29 +0900

    detect log dir writable or not on .start()

--- src/gracenode/index.js
+++ src/gracenode/index.js
@@ -13,6 +13,14 @@ var clusterConfig;
 var modConfigs = {};
 var ready = false;
 
+var ER = {
+	NOT_WRITABLE: '<NOT_WRITABLE>',
+	INVALID_LOG_PATH: '<INVALID_LOG_PATH>',
+};
+var MODE_00200 = 128;
+var MODE_00020 = 16;
+var MODE_00002 = 2;
+
 // a map of bootstrapped modules
 exports.mod = {};
 
@@ -94,11 +102,27 @@ function startCluster(cb) {
 }
 
 function setupLog(cb) {
+	/*
 	if (modConfigs.log && modConfigs.log.file) {
-		fs.exists(modConfigs.log.file, function (exists) {
-			if (!exists) {
-				return cb(new Error('<INVALID_LOG_PATH> ' + modConfigs.log.file));	
+		// validate log path
+		fs.stat(modConfigs.log.file, function (error, stat) {
+			if (error) {
+				return cb(new Error(ER.INVALID_LOG_PATH + ' ' + error.message));	
 			}
+			// check if writable
+			var isOwner = process.uid === stat.uid;
+			var inGroup = process.gid === stat.gid;
+			var mode = stat.mode;
+			var canWrite = isOwner && (mode & MODE_00200) || // is the owner and in group
+				inGroup && (mode & MODE_00020) || // in group
+				(mode & MODE_00002); // anyone can write
+			if (!canWrite) {
+
+				console.log(isOwner, inGroup, mode, process.uid, stat.uid, process.gid, stat.gid);
+				
+				return cb(new Error(ER.NOT_WRITABLE + ' ' + modConfigs.log.file));
+			}
+			// good to go
 			logger = log.create('gracenode');
 			cb();
 		});
@@ -106,6 +130,53 @@ function setupLog(cb) {
 	}
 	logger = log.create('gracenode');
 	cb();
+	*/
+	canWrite(modConfigs.log || null, function (error) {
+		if (error) {
+			return cb(
+				new Error(
+					ER.NOT_WRITABLE + ' ' +
+					modConfigs.log.file + ' ' +
+					error.message
+				)
+			);
+		}
+		logger = log.create('gracenode');
+		cb();
+	});
+}
+
+function canWrite(conf, cb) {
+	if (!conf.file) {
+		cb();
+		return;
+	}
+	fs.open(conf.file, 'w', function (error, fd) {
+		if (error) {
+			var err = null;
+			switch (error.code) {
+				case 'EISDIR':
+					// if we can write a file here, it is good to go
+					try {
+						fs.writeFileSync(conf.file + '/.__');
+						fs.unlinkSync(conf.file + '/.__');
+					} catch (e) {
+						err = e;
+					}
+					break;
+				default:
+					err = error; 
+					break;
+			}
+			return cb(err);
+		}
+		fs.close(fd, function (error) {
+			if (error) {
+				return cb(error);
+			}
+			cb();
+		});
+	});
 }
 
 function setupLogCleaner(cb) {
