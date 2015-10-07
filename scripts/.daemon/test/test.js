f57dc1032f56e60a9ff2420ac9638041238cfc10
voltrue2 <voltrue2@gmail.com>
2015-10-07 18:48:00 +0900

    removed daemon and config under scripts

--- scripts/.daemon/test/test.js
+++
@@ -1,6 +0,0 @@
-var gn = require('../../../');
-gn.setConfigPath('node_modules/gracenode/scripts/daemon/test/');
-gn.setConfigFiles(['conf.json']);
-
-gn.setup(function () {
-});
--- scripts/.daemon/utils/socket-name.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = function (path) {
-	if (!path) {
-		throw new Error('invalid application path given: ' + path);
-	}
-	var sockName = '/tmp/gracenode-monitor-' + path.replace(/\//g, '-') + '.sock';
-	return sockName;
-};
