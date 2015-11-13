e4ae4fb49877a844836b9319207bceeaf22af22e
voltrue2 <voltrue2@gmail.com>
2015-11-13 11:49:58 +0900

    render cache now attempts to discard stale cache data on .set() and .get()

--- src/render/cache.js
+++ src/render/cache.js
@@ -5,6 +5,7 @@ var cache = {};
 // ttl is in ms for how long it should last
 // exmaple if the cache needs to last for 24 hours, then ttl = 8640000
 exports.set = function (data, rendered, ttl) {
+	discard();
 	var key = createKey(data);
 	cache[key] = {
 		rendered: rendered,
@@ -13,6 +14,7 @@ exports.set = function (data, rendered, ttl) {
 };
 
 exports.get = function (data) {
+	discard();
 	var key = createKey(data);
 	if (cache[key]) {
 		var res = cache[key];
@@ -24,6 +26,15 @@ exports.get = function (data) {
 	}
 	return null;
 };
+
+function discard() {
+	for (var key in cache) {
+		var res = cache[key];
+		if (Date.now() >= res.ttile) {
+			delete cache[key];
+		}
+	}
+}
 
 function createKey(data) {
 	return JSON.stringify(data);
