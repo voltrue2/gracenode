3df0d482a59989ee01bb11227088b538a529112b
Nobuyori.Takahashi <no-takahashi>
2018-06-26 11:55:50 +0900

    pre-commit hook added js file change detection

--- src/lint/preCommit.sh
+++ src/lint/preCommit.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-`echo make lint`;
+rootDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+jschanged=`${rootDir}/scripts/detect_js_change`;
 
-exitCode=$?;
-
-if [[ $exitCode != 0 ]]; then
-	exit 1;
+if [ $jschanged -eq 1 ]; then
+    echo "javascript files have been changed: run lint and tests";
+    `echo make lint`;
+    exitCode=$?;
+    if [[ $exitCode != 0 ]]; then
+        exit 1;
+    fi
+    `echo make test`;
+else
+    echo "No javascript files have been changed: skip lint and tests";
 fi
-
-`echo make test`;
